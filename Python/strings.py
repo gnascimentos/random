f383c15748409d47d6cb1efb09ea04295c4c10f1
Gabriel Silveira <gabrielsilveira.sl@hotmail.com>
2023-09-20 17:05:39 -0300

    Create strings.py

---
+++ Python/strings.py
@@ -0,0 +1,21 @@
+Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> nome="gabriel silveira"
+>>> nome[0]
+'g'
+>>> nome[0:5]
+'gabri'
+>>> nome[0:7]
+'gabriel'
+>>> nome*3
+'gabriel silveiragabriel silveiragabriel silveira'
+>>> nome+nome
+'gabriel silveiragabriel silveira'
+>>> "g" in nome
+True
+>>> "z" in nome
+False
+>>> "gb" in nome
+False
+>>> "ga" in nome
+True
